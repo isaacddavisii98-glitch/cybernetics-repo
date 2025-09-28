2112f4fe164e088b8bfcd4eb38365570d12c3b0f
isaacddavisii98-glitch <isaacddavisii98@gmail.com>
2025-09-28 09:12:44 -0500

    Add files via upload

---
+++ IDavis - Lesson 1 Assignment.py
@@ -0,0 +1,17 @@
+Python 3.13.7 (v3.13.7:bcee1c32211, Aug 14 2025, 19:10:51) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> "hello good friends"
+'hello good friends'
+>>> first_name = "Isaac"
+>>> 53/9
+5.888888888888889
+>>> 234/14
+16.714285714285715
+>>> 54 == 9
+False
+>>> 36 == 36
+True
+>>> print
+<built-in function print>
+>>> print ("Completed by Isaac Davis")
+Completed by Isaac Davis
